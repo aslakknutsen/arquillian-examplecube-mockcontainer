92ae9d03aaee820b554cecd4e3eae6a1093d4b82
Aslak Knutsen <aslak@4fs.no>
2015-11-18 11:30:04 +0000

    Use server2 to mock requests to www.google.com

---
+++ src/test/java/org/arquillian/cube/example/mockcontainer/MockContainerTestCase.java
@@ -0,0 +1,28 @@
+package org.arquillian.cube.example.mockcontainer;
+
+import java.net.Socket;
+
+import org.arquillian.cube.HostIp;
+import org.arquillian.cube.docker.impl.util.IOUtil;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class MockContainerTestCase {
+
+    @HostIp
+    private String hostIp;
+    
+    @Test
+    public void shouldMockGoogleCom() throws Exception {
+        Socket socket = new Socket(hostIp, 8085);
+        String response = IOUtil.asString(socket.getInputStream());
+
+        socket.close();
+        
+        System.out.println(response);
+        Assert.assertEquals("INTERCEPTED", response);
+    }
+}
