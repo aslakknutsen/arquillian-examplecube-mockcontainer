1176480359fcda371c3378c7dca1a6cf0cdb2fc9
Aslak Knutsen <aslak@4fs.no>
2015-11-19 12:42:58 +0000

    Update to use MockServer as intercept target
    Allows for Test case control over the response.

--- src/test/java/org/arquillian/cube/example/mockcontainer/MockContainerTestCase.java
+++ src/test/java/org/arquillian/cube/example/mockcontainer/MockContainerTestCase.java
@@ -1,27 +1,51 @@
 package org.arquillian.cube.example.mockcontainer;
 
-import java.net.Socket;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+import java.net.URL;
 
 import org.arquillian.cube.HostIp;
+import org.arquillian.cube.containerobject.HostPort;
 import org.arquillian.cube.docker.impl.util.IOUtil;
 import org.jboss.arquillian.junit.Arquillian;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockserver.client.server.MockServerClient;
 
 @RunWith(Arquillian.class)
 public class MockContainerTestCase {
 
     @HostIp
     private String hostIp;
+
+    private MockServerClient mock;
+
+    @Before
+    public void setupMock() {
+        mock = new MockServerClient(hostIp, 1080);
+        mock
+        .when(
+                request()
+                    .withMethod("GET")
+        ).respond(
+                response()
+                    .withStatusCode(200)
+                    .withBody("INTERCEPTED"));
+    }
+
+    @After
+    public void clear() {
+        mock.clear(null);
+    }
     
     @Test
     public void shouldMockGoogleCom() throws Exception {
-        Socket socket = new Socket(hostIp, 8085);
-        String response = IOUtil.asString(socket.getInputStream());
+        String response = IOUtil.asString(new URL("http", hostIp, 8085, "").openStream());
 
-        socket.close();
-        
         System.out.println(response);
         Assert.assertTrue(response.contains("INTERCEPTED"));
     }
